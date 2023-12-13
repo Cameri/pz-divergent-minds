4bafd34b6e6876232601858ff7b89a24910ea21f
Ricardo Arturo Cabral Mejía <me@ricardocabral.io>
2023-12-13 01:18:26 -0500

    feat: add OCD skeleton

---
+++ Contents/mods/DivergentMinds/media/lua/server/items/DivergentMinds_ProceduralDistributions.lua
@@ -0,0 +1,27 @@
+require "Items/Distributions"
+require "Items/ProceduralDistributions"
+
+local i;
+
+local myDistTable = {
+    "DivergentMinds.PillsProzac", 2,
+}
+local function insertTable(gameList, mylist)
+    local n = #gameList
+    for i = 1, #mylist do
+        gameList[n + i] = mylist[i]
+    end
+end
+
+insertTable(ProceduralDistributions["list"]["ArmyStorageMedical"].items, myDistTable)
+insertTable(ProceduralDistributions["list"]["BathroomCabinet"].items, myDistTable)
+insertTable(ProceduralDistributions["list"]["BathroomCounter"].items, myDistTable)
+insertTable(ProceduralDistributions["list"]["MedicalClinicDrugs"].items, myDistTable)
+insertTable(ProceduralDistributions["list"]["MedicalClinicDrugs"].items, myDistTable)
+insertTable(ProceduralDistributions["list"]["MedicalClinicTools"].items, myDistTable)
+insertTable(ProceduralDistributions["list"]["MedicalStorageDrugs"].items, myDistTable)
+insertTable(ProceduralDistributions["list"]["MedicalStorageDrugs"].items, myDistTable)
+insertTable(ProceduralDistributions["list"]["SafehouseMedical"].items, myDistTable)
+insertTable(ProceduralDistributions["list"]["StoreShelfMedical"].items, myDistTable)
+
+insertTable(ProceduralDistributions["list"]["OfficeDeskHome"].junk.items, myDistTable)
